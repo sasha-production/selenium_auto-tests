f1ce7aaa88362fc2b5818888e5102710d7884c6a
sasha <sasha.ignatov93@gmail.com>
2025-01-23 23:52:31 +0500

    add alert pass

--- pages/main_page.py
+++ pages/main_page.py
@@ -9,8 +9,10 @@ class MainPage(BasePage):
         login_link.click()
         ## Первый способ: возвращать нужный Page Object
         # return LoginPage(browser=self.browser, url=self.browser.current_url)  # явно возвращаем страницу
-
         ## Второй подход: переход происходит неявно, страницу инициализируем в теле теста:
+
+        # alert = self.browser.switch_to.alert # если на странице есть alert
+        # alert.accept()
 
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
