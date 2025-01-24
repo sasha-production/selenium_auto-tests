4ee7bfea0b46b85fba3eaf8dd80a4c532ff11698
sasha <sasha.ignatov93@gmail.com>
2025-01-25 00:51:54 +0500

    parametrization with xfail(or zskip) to find bugged link

--- pages/product_page.py
+++ pages/product_page.py
@@ -3,14 +3,20 @@ from .locators import ProductPageLocators
 
 
 class ProductPage(BasePage):
+    basket_total: float
 
     def product_title(self):
         product_title = self.browser.find_element(*ProductPageLocators.PRODUCT_TITLE).text
         return product_title
 
     def product_price(self):
-        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text.strip().replace('£', '')
         return product_price
+
+    # def current_basket_total(self):
+    #     basket_total = self.browser.find_element(*ProductPageLocators.BASKET_TOTAL).strip().replace('£', '')
+    #     self.basket_total = float(basket_total)
+    #     print(basket_total)
 
     def should_add_product_to_basket(self):
         basket_button = self.browser.find_element(*ProductPageLocators.BASKET_BUTTON)
@@ -18,7 +24,7 @@ class ProductPage(BasePage):
 
     def should_be_title_and_price_after_adding_to_basket(self):
         self.should_be_product_title_on_page_after_add_to_basket()
-        self.should_be_equal_product_price_and_basket_cost()
+        # self.should_be_equal_product_price_and_basket_cost()
 
     def should_be_product_title_on_page_after_add_to_basket(self):
         # Сначала проверяем, что элементы присутствуют на странице
@@ -30,6 +36,6 @@ class ProductPage(BasePage):
     def should_be_equal_product_price_and_basket_cost(self):
         # Сначала проверяем, что элементы присутствуют на странице
         assert self.is_element_present(*ProductPageLocators.BASKET_COST)
-        basket_cost = self.browser.find_element(*ProductPageLocators.BASKET_COST).text
+        basket_cost = self.browser.find_element(*ProductPageLocators.BASKET_COST).text.strip().replace('£', '')
         product_price = self.product_price()
-        assert basket_cost == product_price, "Цена товара и стоимость корзины не совпадают"
+        assert basket_cost == product_price + self.basket_total, "Цена товара и стоимость корзины не совпадают"
--- test_product_page.py
+++ test_product_page.py
@@ -1,12 +1,29 @@
+import time
+
+import pytest
+
 from pages.product_page import ProductPage
 
+links = ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+         pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                      marks=pytest.mark.xfail),
+         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"]
 
-def test_guest_should_add_product_to_basket(browser):
+
+@pytest.mark.parametrize('link', links)
+def test_guest_should_add_product_to_basket(browser, link):
     # link = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=newYear'
-    link2 = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
-    page = ProductPage(browser, link2)
+    page = ProductPage(browser, link)
     page.open()
+    # page.current_basket_total() # не понадобился, сравнивается только заголовок
     page.should_add_product_to_basket()
     page.solve_quiz_and_get_code()
     page.should_be_title_and_price_after_adding_to_basket()
-
+    time.sleep(2)
